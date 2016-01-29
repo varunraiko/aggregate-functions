DanielDent/server

MDEV-9058: protocol: COM_MULTI command (part 3)
Support of "previuousely used statement ID".
All IDs with highest bit ON reserved for special use.

sql/sql_prepare.cc
@@ -324,7 +324,9 @@ find_prepared_statement(THD *thd, ulong id)
     prepared statements find() will return 0 if there is a named prepared
     statement with such id.
   */
-  Statement *stmt= thd->stmt_map.find(id);
+  Statement *stmt= ((id == PREV_STMT_ID) ?
+                    thd->last_stmt :
+                    thd->stmt_map.find(id));
 
   if (stmt == 0 || stmt->type() != Query_arena::PREPARED_STATEMENT)
     return NULL;
@@ -2335,6 +2337,8 @@ void mysqld_stmt_prepare(THD *thd, const char *packet, uint packet_length)
     /* Statement map deletes statement on erase */
     thd->stmt_map.erase(stmt);
   }
+  else
+    thd->set_last_stmt(stmt);
 
   thd->protocol= save_protocol;
 
@@ -2730,6 +2734,8 @@ void mysqld_stmt_execute(THD *thd, char *packet_arg, uint packet_length)
   sp_cache_enforce_limit(thd->sp_proc_cache, stored_program_cache_size);
   sp_cache_enforce_limit(thd->sp_func_cache, stored_program_cache_size);
 
+  thd->set_last_stmt(stmt);
+
   /* Close connection socket; for use with client testing (Bug#43560). */
   DBUG_EXECUTE_IF("close_conn_after_stmt_execute", vio_close(thd->net.vio););
 
@@ -2833,6 +2839,8 @@ void mysqld_stmt_fetch(THD *thd, char *packet, uint packet_length)
     reset_stmt_params(stmt);
   }
 
+  thd->set_last_stmt(stmt);
+
   thd->restore_backup_statement(stmt, &stmt_backup);
   thd->stmt_arena= thd;
 
@@ -2889,6 +2897,8 @@ void mysqld_stmt_reset(THD *thd, char *packet)
 
   my_ok(thd);
 
+  thd->set_last_stmt(stmt);
+
   DBUG_VOID_RETURN;
 }
 
@@ -2919,6 +2929,8 @@ void mysqld_stmt_close(THD *thd, char *packet)
   DBUG_ASSERT(! stmt->is_in_use());
   stmt->deallocate();
   general_log_print(thd, thd->get_command(), NullS);
+
+  thd->no_last_stmt();
 
   DBUG_VOID_RETURN;
 }
@@ -3028,6 +3040,8 @@ void mysql_stmt_get_longdata(THD *thd, char *packet, ulong packet_length)
     stmt->last_errno= thd->get_stmt_da()->sql_errno();
     strncpy(stmt->last_error, thd->get_stmt_da()->message(), MYSQL_ERRMSG_SIZE);
   }
+  else
+    thd->set_last_stmt(stmt);
   thd->set_stmt_da(save_stmt_da);
 
   general_log_print(thd, thd->get_command(), NullS);
@@ -3182,7 +3196,8 @@ end:
 
 Prepared_statement::Prepared_statement(THD *thd_arg)
   :Statement(NULL, &main_mem_root,
-             STMT_INITIALIZED, ++thd_arg->statement_id_counter),
+             STMT_INITIALIZED,
+             ((++thd_arg->statement_id_counter) & STMT_ID_MASK)),
   thd(thd_arg),
   result(thd_arg),
   param_array(0),

sql/sql_prepare.h
@@ -18,6 +18,10 @@
 
 #include "sql_error.h"
 
+
+#define PREV_STMT_ID 0xFFFFFFFF
+#define STMT_ID_MASK 0x7FFFFFFF
+
 class THD;
 struct LEX;
 

varunraiko/aggregate-functions

sql/sql_prepare.cc
@@ -324,7 +324,9 @@ find_prepared_statement(THD *thd, ulong id)
     prepared statements find() will return 0 if there is a named prepared
     statement with such id.
   */
-  Statement *stmt= thd->stmt_map.find(id);
+  Statement *stmt= ((id == LAST_STMT_ID) ?
+                    thd->last_stmt :
+                    thd->stmt_map.find(id));
 
   if (stmt == 0 || stmt->type() != Query_arena::PREPARED_STATEMENT)
     return NULL;
@@ -2322,7 +2324,10 @@ void mysqld_stmt_prepare(THD *thd, const char *packet, uint packet_length)
   {
     /* Statement map deletes statement on erase */
     thd->stmt_map.erase(stmt);
+    thd->clear_last_stmt();
   }
+  else
+    thd->set_last_stmt(stmt);
 
   thd->protocol= save_protocol;
 
@@ -2908,6 +2913,9 @@ void mysqld_stmt_close(THD *thd, char *packet)
   stmt->deallocate();
   general_log_print(thd, thd->get_command(), NullS);
 
+  if (thd->last_stmt == stmt)
+    thd->clear_last_stmt();
+
   DBUG_VOID_RETURN;
 }
 
@@ -3170,7 +3178,8 @@ end:
 
 Prepared_statement::Prepared_statement(THD *thd_arg)
   :Statement(NULL, &main_mem_root,
-             STMT_INITIALIZED, ++thd_arg->statement_id_counter),
+             STMT_INITIALIZED,
+             ((++thd_arg->statement_id_counter) & STMT_ID_MASK)),
   thd(thd_arg),
   result(thd_arg),
   param_array(0),

sql/sql_prepare.h
@@ -18,6 +18,10 @@
 
 #include "sql_error.h"
 
+
+#define LAST_STMT_ID 0xFFFFFFFF
+#define STMT_ID_MASK 0x7FFFFFFF
+
 class THD;
 struct LEX;
 