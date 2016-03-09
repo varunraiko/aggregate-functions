5f009ac67a64a64031680e65f46acf822e99fe60
Sergei Golubchik <serg@mariadb.org>
2016-03-09 12:32:35 +0100

    cleanup: remove unused Field::utype values
    and FIELDFLAG_xxx constants

--- sql/sql_table.cc
+++ sql/sql_table.cc
@@ -2902,7 +2902,6 @@ int prepare_create_field(Column_definition *sql_field,
     if (sql_field->charset->state & MY_CS_BINSORT)
       sql_field->pack_flag|=FIELDFLAG_BINARY;
     sql_field->length=8;			// Unireg field length
-    sql_field->unireg_check=Field::BLOB_FIELD;
     (*blob_columns)++;
     break;
   case MYSQL_TYPE_GEOMETRY:
@@ -2919,7 +2918,6 @@ int prepare_create_field(Column_definition *sql_field,
     if (sql_field->charset->state & MY_CS_BINSORT)
       sql_field->pack_flag|=FIELDFLAG_BINARY;
     sql_field->length=8;			// Unireg field length
-    sql_field->unireg_check=Field::BLOB_FIELD;
     (*blob_columns)++;
     break;
 #else
@@ -2956,7 +2954,6 @@ int prepare_create_field(Column_definition *sql_field,
       FIELDFLAG_INTERVAL;
     if (sql_field->charset->state & MY_CS_BINSORT)
       sql_field->pack_flag|=FIELDFLAG_BINARY;
-    sql_field->unireg_check=Field::INTERVAL_FIELD;
     if (check_duplicates_in_interval("ENUM",sql_field->field_name,
                                      sql_field->interval,
                                      sql_field->charset, &dup_val_count))
@@ -2967,7 +2964,6 @@ int prepare_create_field(Column_definition *sql_field,
       FIELDFLAG_BITFIELD;
     if (sql_field->charset->state & MY_CS_BINSORT)
       sql_field->pack_flag|=FIELDFLAG_BINARY;
-    sql_field->unireg_check=Field::BIT_FIELD;
     if (check_duplicates_in_interval("SET",sql_field->field_name,
                                      sql_field->interval,
                                      sql_field->charset, &dup_val_count))
